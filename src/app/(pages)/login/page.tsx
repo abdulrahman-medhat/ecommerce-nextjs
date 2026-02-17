dce609ef5df06f831ca2681e273ef19d1606c515
Abdulrhaman25 <https://github.com/Abdulrhaman25/onep.git>
2026-02-17 04:10:53 +0200

    تحديث الملفات الأخيرة

--- src/app/(pages)/login/page.tsx
+++ src/app/(pages)/login/page.tsx
@@ -1,13 +1,17 @@
 "use client";
 
+import { Suspense } from "react";
 import LoginForm from "../../component/LoginForm";
 
 
 
 export default function login() {
   return (
+    <Suspense>
+
     <div className="min-h-screen flex items-center justify-center bg-gray-100">
       <LoginForm />
     </div>
+    </Suspense>
   );
 }
